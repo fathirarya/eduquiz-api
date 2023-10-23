66708a42ef737e6000b5429ee160f7fb292dde06
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-23 13:46:27 +0700

    init echo server withj mysql setup

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"eduquiz-api/config"
+	"eduquiz-api/routes"
+	"net/http"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	myApp := echo.New()
+	validate := validator.New()
+
+	DB := config.ConnectDB()
+
+	myApp.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to EduQuiz API Services")
+	})
+
+	routes.UserRoutes(myApp, DB, validate)
+
+	myApp.Pre(middleware.RemoveTrailingSlash())
+	myApp.Use(middleware.CORS())
+	myApp.Use(middleware.LoggerWithConfig(
+		middleware.LoggerConfig{
+			Format: "method=${method}, uri=${uri}, status=${status}, time=${time_rfc3339}\n",
+		},
+	))
+
+	myApp.Logger.Fatal(myApp.Start(":8080"))
+}
