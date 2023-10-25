eb2ac8b24fe6b707c123c92e7a6f552323c3a70d
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-25 14:31:58 +0700

    add quiz categories utils response

---
+++ utils/res/quizCategoriesRes.go
@@ -0,0 +1,33 @@
+package res
+
+import (
+	"eduquiz-api/model/domain"
+	"eduquiz-api/model/schema"
+	"eduquiz-api/model/web"
+)
+
+func QuizCategorySchemaToQuizCategoryDomain(quizCategory *schema.QuizCategory) *domain.QuizCategory {
+	return &domain.QuizCategory{
+		ID:       quizCategory.ID,
+		Category: quizCategory.Category,
+	}
+}
+
+func QuizCategoryDomainToQuizCategoryResponse(quizCategory *domain.QuizCategory) web.QuizCategoryResponse {
+	return web.QuizCategoryResponse{
+		ID:       quizCategory.ID,
+		Category: quizCategory.Category,
+	}
+}
+
+func ConvertQuizCategoryResponse(quizCategory []domain.QuizCategory) []web.QuizCategoryResponse {
+	var results []web.QuizCategoryResponse
+	for _, quizCategory := range quizCategory {
+		quizCategoryResponse := web.QuizCategoryResponse{
+			ID:       quizCategory.ID,
+			Category: quizCategory.Category,
+		}
+		results = append(results, quizCategoryResponse)
+	}
+	return results
+}
