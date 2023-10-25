17c2ce414e48eb84a9123659069f9c2fafefc37b
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-26 00:27:29 +0700

    add quiz request web

---
+++ model/web/quizRequest.go
@@ -0,0 +1,13 @@
+package web
+
+type QuizCreateRequest struct {
+	Title          string `json:"title" validate:"required,min=1,max=255"`
+	Description    string `json:"description" validate:"required,min=1,max=255"`
+	QuizCategoryID uint   `json:"quiz_category_id" validate:"required"`
+}
+
+type QuizUpdateRequest struct {
+	Title          string `json:"title" validate:"required,min=1,max=255"`
+	Description    string `json:"description" validate:"required,min=1,max=255"`
+	QuizCategoryID uint   `json:"quiz_category_id" validate:"required"`
+}
