487f485d1195b95077e3d47367e202d1f9b29f7c
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-26 22:24:32 +0700

    update quiz request web

--- model/web/quizRequest.go
+++ model/web/quizRequest.go
@@ -7,7 +7,6 @@ type QuizCreateRequest struct {
 }
 
 type QuizUpdateRequest struct {
-	Title          string `json:"title" validate:"required,min=1,max=255"`
-	Description    string `json:"description" validate:"required,min=1,max=255"`
-	QuizCategoryID uint   `json:"quiz_category_id" validate:"required"`
+	Title       string `json:"title" validate:"required,min=1,max=255"`
+	Description string `json:"description" validate:"required,min=1,max=255"`
 }
