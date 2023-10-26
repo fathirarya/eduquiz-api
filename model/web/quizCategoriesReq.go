1664d296cfc136de044784c9eea9d6700141f89d
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-26 22:48:45 +0700

    update quiz categories request web

--- model/web/quizCategoriesReq.go
+++ model/web/quizCategoriesReq.go
@@ -2,5 +2,5 @@ package web
 
 type QuizCategoryCreateRequest struct {
 	ID       uint   `json:"id"`
-	Category string `json:"name" validate:"required,min=1,max=255"`
+	Category string `json:"category" validate:"required,min=1,max=255"`
 }
