6ee7eed069bf80e7f8380deb98eb679c9708571b
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-27 18:42:11 +0700

    add question domain

---
+++ model/domain/questionDomain.go
@@ -0,0 +1,12 @@
+package domain
+
+type Question struct {
+	ID       uint
+	QuizID   uint
+	Quiz     Quiz
+	Question string
+	Option1  string
+	Option2  string
+	Option3  string
+	Option4  string
+}
