680112564bafde8eff42e783fac773df810cdc08
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-27 18:41:08 +0700

    add migration question config

--- config/database.go
+++ config/database.go
@@ -43,5 +43,5 @@ func ConnectDB() *gorm.DB {
 }
 
 func Migrate() {
-	DB.AutoMigrate(schema.Users{}, schema.QuizCategory{}, schema.Quiz{})
+	DB.AutoMigrate(schema.Users{}, schema.QuizCategory{}, schema.Quiz{}, schema.Question{})
 }
