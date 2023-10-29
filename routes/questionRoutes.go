4990d0a95d58431c48b900c0ff7fb044c9f44b38
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-29 21:42:41 +0700

    add question routes

--- routes/questionRoutes.go
+++ routes/questionRoutes.go
@@ -4,8 +4,11 @@ import (
 	"eduquiz-api/controller"
 	"eduquiz-api/repository"
 	"eduquiz-api/service"
+	"eduquiz-api/utils/helper/middleware"
+	"os"
 
 	"github.com/go-playground/validator"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
@@ -16,11 +19,12 @@ func QuestionRoutes(e *echo.Echo, db *gorm.DB, validate *validator.Validate) {
 	questionController := controller.NewQuestionController(questionService)
 
 	questionGroup := e.Group("api/v1/question")
+	questionGroup.Use(echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))))
 
-	questionGroup.POST("", questionController.CreateQuestionController)
+	questionGroup.POST("", questionController.CreateQuestionController, middleware.AuthMiddleware("Guru"))
 	questionGroup.GET("/:id", questionController.GetQuestionByIdController)
 	questionGroup.GET("", questionController.GetAllQuestionController)
-	questionGroup.PUT("/:id", questionController.UpdateQuestionController)
-	questionGroup.DELETE("/:id", questionController.DeleteQuestionController)
+	questionGroup.PUT("/:id", questionController.UpdateQuestionController, middleware.AuthMiddleware("Guru"))
+	questionGroup.DELETE("/:id", questionController.DeleteQuestionController, middleware.AuthMiddleware("Guru"))
 
 }
