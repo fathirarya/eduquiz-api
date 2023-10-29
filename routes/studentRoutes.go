ca28a7c3119b0976e850a0f17450e3a21e0a7f39
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-29 21:28:56 +0700

    update student routes

---
+++ routes/studentRoutes.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"eduquiz-api/controller"
+	"eduquiz-api/repository"
+	"eduquiz-api/service"
+	"eduquiz-api/utils/helper/middleware"
+	"os"
+
+	"github.com/go-playground/validator"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func StudentRoutes(e *echo.Echo, db *gorm.DB, validate *validator.Validate) {
+
+	studentRepository := repository.NewStudentRepository(db)
+	studentService := service.NewStudentService(studentRepository, validate)
+	studentController := controller.NewStudentController(studentService)
+
+	studentsGroup := e.Group("api/v1/students")
+	studentsGroup.POST("/register", studentController.RegisterStudentController)
+	studentsGroup.POST("/login", studentController.LoginStudentController)
+
+	studentsGroup.Use(echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))))
+
+	studentsGroup.GET("/:id", studentController.GetStudentController, middleware.AuthMiddleware("Siswa"))
+	studentsGroup.GET("", studentController.GetStudentsController, middleware.AuthMiddleware("Siswa"))
+	studentsGroup.GET("/:name", studentController.GetStudentByNameController, middleware.AuthMiddleware("Siswa"))
+	studentsGroup.PUT("/:id", studentController.UpdateStudentController, middleware.AuthMiddleware("Siswa"))
+	studentsGroup.DELETE("/:id", studentController.DeleteStudentController, middleware.AuthMiddleware("Siswa"))
+
+}
