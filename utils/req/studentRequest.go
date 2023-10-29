13e335a7dcf6c0d93107490a830959d2048d61c0
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-29 21:29:18 +0700

    update student request utils

--- utils/req/studentRequest.go
+++ utils/req/studentRequest.go
@@ -6,18 +6,8 @@ import (
 	"eduquiz-api/model/web"
 )
 
-func UserCreateRequestToUserDomain(request web.UserCreateRequest) *domain.Users {
-	return &domain.Users{
-		Username: request.Username,
-		Password: request.Password,
-		Email:    request.Email,
-		Fullname: request.Fullname,
-		Roles:    request.Roles,
-	}
-}
-
-func UserUpdateRequestToUserDomain(request web.UserUpdateRequest) *domain.Users {
-	return &domain.Users{
+func StudentCreateRequestToStudentDomain(request web.StudentCreateRequest) *domain.Student {
+	return &domain.Student{
 		Username: request.Username,
 		Password: request.Password,
 		Email:    request.Email,
@@ -25,20 +15,28 @@ func UserUpdateRequestToUserDomain(request web.UserUpdateRequest) *domain.Users 
 	}
 }
 
-func UserLoginRequestToUserDomain(request web.UserLoginRequest) *domain.Users {
-	return &domain.Users{
+func StudentUpdateRequestToStudentDomain(request web.StudentUpdateRequest) *domain.Student {
+	return &domain.Student{
+		Username: request.Username,
+		Password: request.Password,
+		Email:    request.Email,
+		Fullname: request.Fullname,
+	}
+}
+
+func StudentLoginRequestToStudentDomain(request web.StudentLoginRequest) *domain.Student {
+	return &domain.Student{
 		Email:    request.Email,
 		Password: request.Password,
 	}
 }
 
-func UserDomainToUserSchema(user domain.Users) *schema.Users {
-	return &schema.Users{
+func StudentDomainToStudentSchema(user domain.Student) *schema.Student {
+	return &schema.Student{
 		ID:       user.ID,
 		Username: user.Username,
 		Password: user.Password,
 		Email:    user.Email,
 		Fullname: user.Fullname,
-		Roles:    user.Roles,
 	}
 }
