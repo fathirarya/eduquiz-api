44101b84beeb1d55212f6dc417053d6e3283ac27
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 01:45:06 +0700

    update quiz response web

--- model/web/quizResponse.go
+++ model/web/quizResponse.go
@@ -1,9 +1,19 @@
 package web
 
+import "time"
+
 type QuizResponse struct {
-	ID             uint   `json:"id"`
-	QuizCategoryID uint   `json:"quiz_category_id"`
-	Category       string `json:"category"`
-	Title          string `json:"title"`
-	Description    string `json:"description"`
+	ID             uint      `json:"id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	TeacherID      uint      `json:"teacher_id"`
+	FullName       string    `json:"full_name"`
+	QuizCategoryID uint      `json:"quiz_category_id"`
+	Category       string    `json:"category"`
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+}
+
+type ImplementationOpenAiResponse struct {
+	OutputMessage string `json:"output_message"`
 }
