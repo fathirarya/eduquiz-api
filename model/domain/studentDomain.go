d0bca237c5f2766f5dfbf5953c86c156044cfc2e
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-31 19:27:28 +0700

    update student domain

--- model/domain/studentDomain.go
+++ model/domain/studentDomain.go
@@ -1,9 +1,14 @@
 package domain
 
+import "time"
+
 type Student struct {
-	ID       uint
-	Username string
-	Password string
-	Email    string
-	Fullname string
+	ID        uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeleteAt  time.Time
+	Username  string
+	Password  string
+	Email     string
+	Fullname  string
 }
