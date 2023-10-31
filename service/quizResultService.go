f1d0a46393ec1b302cde7383a37aaff057dd19a7
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 03:51:58 +0700

    update quiz result service

--- service/quizResultService.go
+++ service/quizResultService.go
@@ -16,16 +16,56 @@ type QuizResultService interface {
 type QuizResultServiceImpl struct {
 	QuizResultRepository   repository.QuizResultRepository
 	AttemptAnswerRepostory repository.AttemptAnswerRepository
+	QuestionRepository     repository.QuestionRepository
 	Validate               *validator.Validate
 }
 
-func NewQuizResultService(quizResultRepository repository.QuizResultRepository, attemptRepository repository.AttemptAnswerRepository, validate *validator.Validate) *QuizResultServiceImpl {
+func NewQuizResultService(quizResultRepository repository.QuizResultRepository, attemptRepository repository.AttemptAnswerRepository, questionRepository repository.QuestionRepository, validate *validator.Validate) *QuizResultServiceImpl {
 	return &QuizResultServiceImpl{
 		QuizResultRepository:   quizResultRepository,
 		AttemptAnswerRepostory: attemptRepository,
+		QuestionRepository:     questionRepository,
 		Validate:               validate,
 	}
 }
 
-// func (service *QuizResultServiceImpl) PostResult(ctx echo.Context, request web.QuizResultCreateRequest) (*domain.QuizResult, error) {
-// }
+func (service *QuizResultServiceImpl) PostResult(ctx echo.Context, request web.QuizResultCreateRequest) (*domain.QuizResult, error) {
+	err := service.Validate.Struct(request)
+	if err != nil {
+		return nil, err
+	}
+
+	score, err := service.calculateScore(request)
+	if err != nil {
+		return nil, err
+	}
+
+	quizResult := domain.QuizResult{
+		StudentID: request.StudentID,
+		QuizID:    request.QuizID,
+		Score:     int(score),
+	}
+
+	return service.QuizResultRepository.PostResult(&quizResult)
+}
+
+func (service *QuizResultServiceImpl) calculateScore(request web.QuizResultCreateRequest) (float64, error) {
+	attemptAnswers, err := service.AttemptAnswerRepostory.FindByStudentId(int(request.StudentID))
+	if err != nil {
+		return 0, err
+	}
+
+	var correctAnswers int
+
+	for _, attemptAnswer := range attemptAnswers {
+		if attemptAnswer.IsCorrect {
+			correctAnswers++
+		}
+	}
+
+	totalQuestions, err := service.QuestionRepository.FindByQuizId(int(request.QuizID))
+
+	score := float64(correctAnswers) / float64(len(totalQuestions)) * 100
+
+	return score, nil
+}
