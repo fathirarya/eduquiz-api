5677171732b6e749446667a8ec8803661ed5d714
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 01:43:53 +0700

    update quiz schema

--- model/schema/quizSchema.go
+++ model/schema/quizSchema.go
@@ -8,7 +8,8 @@ type Quiz struct {
 	gorm.Model
 	QuizCategoryID uint         `gorm:"index"`
 	QuizCategory   QuizCategory `gorm:"foreignkey:QuizCategoryID"`
+	TeacherID      uint         `gorm:"index"`
+	Teacher        Teacher      `gorm:"foreignkey:TeacherID"`
 	Title          string       `gorm:"type:varchar(255);not null"`
 	Description    string       `gorm:"type:varchar(255);not null"`
-	QuizResults    []QuizResult `gorm:"foreignKey:QuizID"`
 }
