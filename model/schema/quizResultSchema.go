38b241948758d51a8e967d5e2c65ea7ca7d58689
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 03:49:32 +0700

    quiz result schema

--- model/schema/quizResultSchema.go
+++ model/schema/quizResultSchema.go
@@ -8,5 +8,5 @@ type QuizResult struct {
 	Student   Student `gorm:"foreignkey:StudentID"`
 	QuizID    uint    `gorm:"index"`
 	Quiz      Quiz    `gorm:"foreignkey:QuizID"`
-	Score     uint    `gorm:"type:int;not null"`
+	Score     int     `gorm:"type:int;not null"`
 }
