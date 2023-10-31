d465612b05483b337e90b74d6cbf635cb091715f
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-31 19:27:06 +0700

    update attempt answer domain

--- model/domain/attemptAnswerDomain.go
+++ model/domain/attemptAnswerDomain.go
@@ -2,9 +2,12 @@ package domain
 
 type AttemptAnswer struct {
 	ID         uint
+	StudentID  uint
+	Student    Student
+	QuizID     uint
+	Quiz       Quiz
 	QuestionID uint
 	Question   Question
 	Answer     string
 	IsCorrect  bool
-	// Quest      string
 }
