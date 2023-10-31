7b4466a219989a38b85ab984895360b965bc4437
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 01:43:16 +0700

    update quiz domain

--- model/domain/quizDomain.go
+++ model/domain/quizDomain.go
@@ -1,10 +1,17 @@
 package domain
 
+import "time"
+
 type Quiz struct {
 	ID             uint
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	TeacherID      uint
+	Teacher        Teacher
 	QuizCategoryID uint
 	QuizCategory   QuizCategory
 	Title          string
 	Description    string
 	Category       string
+	FullName       string
 }
