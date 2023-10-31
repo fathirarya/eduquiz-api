b7608d39cc07df0b4986947f5bb00fd2f710c72e
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 02:02:51 +0700

    add quiz result response utils

---
+++ utils/res/quizResultRes.go
@@ -0,0 +1,40 @@
+package res
+
+import (
+	"eduquiz-api/model/domain"
+	"eduquiz-api/model/schema"
+	"eduquiz-api/model/web"
+)
+
+func QuizResultSchemaToQuizResultDomain(quizResult *schema.QuizResult) *domain.QuizResult {
+	return &domain.QuizResult{
+		StudentID: quizResult.StudentID,
+		QuizID:    quizResult.QuizID,
+		Score:     quizResult.Score,
+	}
+}
+
+func QuizResultDomainToQuizResultResponse(quizResult *domain.QuizResult) web.QuizResultResponse {
+	return web.QuizResultResponse{
+		StudentID: quizResult.StudentID,
+		FullName:  quizResult.FullName,
+		QuizID:    quizResult.QuizID,
+		Title:     quizResult.Title,
+		Score:     quizResult.Score,
+	}
+}
+
+func ConvertQuizResultResponse(quizResults []domain.QuizResult) []web.QuizResultResponse {
+	var results []web.QuizResultResponse
+	for _, quizResult := range quizResults {
+		quizResultResponse := web.QuizResultResponse{
+			StudentID: quizResult.StudentID,
+			FullName:  quizResult.FullName,
+			QuizID:    quizResult.QuizID,
+			Title:     quizResult.Title,
+			Score:     quizResult.Score,
+		}
+		results = append(results, quizResultResponse)
+	}
+	return results
+}
