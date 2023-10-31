5fd1bd86de5709ec2fc2ef282033e44839262a38
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-31 19:27:57 +0700

    update attempt answer schema

--- model/schema/attemptSchema.go
+++ model/schema/attemptSchema.go
@@ -2,6 +2,10 @@ package schema
 
 type AttemptAnswer struct {
 	ID         uint     `json:"id" gorm:"primaryKey"`
+	StudentID  uint     `json:"student_id"`
+	Student    Student  `json:"student" gorm:"foreignKey:StudentID"`
+	QuizID     uint     `json:"quiz_id"`
+	Quiz       Quiz     `json:"quiz" gorm:"foreignKey:QuizID"`
 	QuestionID uint     `json:"question_id"`
 	Question   Question `json:"question" gorm:"foreignKey:QuestionID"`
 	Answer     string   `json:"answer"`
