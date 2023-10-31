9d5e3ac8ebec2d551f5819f9ef2c1f73e5e4a622
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-10-31 19:34:16 +0700

    update attempt answer response web

--- model/web/attemptAnswerRes.go
+++ model/web/attemptAnswerRes.go
@@ -1,9 +1,10 @@
 package web
 
 type AttemptAnswerResponse struct {
-	ID         uint `json:"id"`
-	QuestionID uint `json:"question_id"`
-	// Quest      string `json:"quest"`
-	Answer    string `json:"answer"`
-	IsCorrect bool   `json:"is_correct"`
+	ID         uint   `json:"id"`
+	StudentID  uint   `json:"student_id"`
+	QuizID     uint   `json:"quiz_id"`
+	QuestionID uint   `json:"question_id"`
+	Answer     string `json:"answer"`
+	IsCorrect  bool   `json:"is_correct"`
 }
