e05a3a5cc1eaafdccfe643c8d4bbe80c00b6cf5e
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 01:44:45 +0700

    update quiz request web

--- model/web/quizRequest.go
+++ model/web/quizRequest.go
@@ -4,9 +4,14 @@ type QuizCreateRequest struct {
 	Title          string `json:"title" validate:"required,min=1,max=255"`
 	Description    string `json:"description" validate:"required,min=1,max=255"`
 	QuizCategoryID uint   `json:"quiz_category_id" validate:"required"`
+	TeacherID      uint   `json:"teacher_id" validate:"required"`
 }
 
 type QuizUpdateRequest struct {
 	Title       string `json:"title" validate:"required,min=1,max=255"`
 	Description string `json:"description" validate:"required,min=1,max=255"`
+}
+
+type ImplementationOpenAiRequest struct {
+	InputMessage string `json:"input_message" validate:"required,min=1,max=255"`
 }
