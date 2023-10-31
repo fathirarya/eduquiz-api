807c0ec7539048be2988f9b601df8fc72cb80646
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 03:51:45 +0700

    update quiz result routes

--- routes/quizResultRoutes.go
+++ routes/quizResultRoutes.go
@@ -1,17 +1,30 @@
 package routes
 
-// func QuizResultRoutes(e *echo.Echo, DB *gorm.DB, validate *validator.Validate) {
-// 	attemptAnswerRepository := repository.NewAttemptAnswerRepository(DB)
-// 	keyAnswerRepository := repository.NewKeyAnswerRepository(DB)
-// 	attemptAnswerService := service.NewAttemptAnswerService(attemptAnswerRepository, keyAnswerRepository, validate)
+import (
+	"eduquiz-api/controller"
+	"eduquiz-api/repository"
+	"eduquiz-api/service"
+	"os"
 
-// 	quizResultRepository := repository.NewQuizResultRepository(DB)
-// 	quizResultService := service.NewQuizResultService(quizResultRepository, attemptAnswerRepository, validate)
-// 	// quizResultController := controller.NewQuizResultController(quizResultService, attemptAnswerService)
+	"github.com/go-playground/validator"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
 
-// 	quizResult := e.Group("api/v1/quizresult")
+func QuizResultRoutes(e *echo.Echo, DB *gorm.DB, validate *validator.Validate) {
+	attemptAnswerRepository := repository.NewAttemptAnswerRepository(DB)
+	keyAnswerRepository := repository.NewKeyAnswerRepository(DB)
+	questionRepository := repository.NewQuestionRepository(DB)
+	attemptAnswerService := service.NewAttemptAnswerService(attemptAnswerRepository, keyAnswerRepository, validate)
 
-// 	quizResult.Use(echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))))
+	quizResultRepository := repository.NewQuizResultRepository(DB)
+	quizResultService := service.NewQuizResultService(quizResultRepository, attemptAnswerRepository, questionRepository, validate)
+	quizResultController := controller.NewQuizResultController(quizResultService, attemptAnswerService)
 
-// 	quizResult.POST("", quizResultController.CreateQuizResultController)
-// }
+	quizResult := e.Group("api/v1/quizresult")
+
+	quizResult.Use(echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))))
+
+	quizResult.POST("", quizResultController.CreateQuizResultController)
+}
