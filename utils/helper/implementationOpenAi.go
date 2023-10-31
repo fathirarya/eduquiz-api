43d886e71eb2dee8beaad359f3ad8591376f86c3
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 01:52:17 +0700

    add implementasi open ai

---
+++ utils/helper/implementationOpenAi.go
@@ -0,0 +1,50 @@
+package helper
+
+import (
+	"context"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+func DiagnosticAI(userInput, openAIKey string) (string, error) {
+	ctx := context.Background()
+	client := openai.NewClient(openAIKey)
+	model := openai.GPT3Dot5Turbo
+	messages := []openai.ChatCompletionMessage{
+		{
+			Role:    openai.ChatMessageRoleSystem,
+			Content: "Siswa menanyakan soal. Jawablah dengan materi tentang soal dan jangan berikan jawaban secara langsung",
+		},
+		{
+			Role:    openai.ChatMessageRoleUser,
+			Content: userInput,
+		},
+	}
+
+	resp, err := getCompletionFromMessages(ctx, client, messages, model)
+	if err != nil {
+		return "", err
+	}
+	answer := resp.Choices[0].Message.Content
+	return answer, nil
+}
+
+func getCompletionFromMessages(
+	ctx context.Context,
+	client *openai.Client,
+	messages []openai.ChatCompletionMessage,
+	model string,
+) (openai.ChatCompletionResponse, error) {
+	if model == "" {
+		model = openai.GPT3Dot5Turbo
+	}
+
+	resp, err := client.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model:    model,
+			Messages: messages,
+		},
+	)
+	return resp, err
+}
