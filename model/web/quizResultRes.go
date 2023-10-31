879155bc71fc03c108d53f1dfe4b428e50b41a16
M. Fathir Arya Nafis <fathirarya2002@gmail.com>
2023-11-01 04:32:16 +0700

    update quiz result response web

--- model/web/quizResultRes.go
+++ model/web/quizResultRes.go
@@ -1,10 +1,8 @@
 package web
 
 type QuizResultResponse struct {
-	ID        uint   `json:"id"`
-	StudentID uint   `json:"student_id"`
-	FullName  string `json:"full_name"`
-	QuizID    uint   `json:"quiz_id"`
-	Title     string `json:"title"`
-	Score     int    `json:"score"`
+	ID        uint `json:"id"`
+	StudentID uint `json:"student_id"`
+	QuizID    uint `json:"quiz_id"`
+	Score     int  `json:"score"`
 }
